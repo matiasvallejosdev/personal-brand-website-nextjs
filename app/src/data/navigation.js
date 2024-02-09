c06c715dd02d6577d55bd92d58902f689e53514e
Matias Vallejos <matiasvallejosdev@outlook.com>
2024-02-09 18:21:04 -0300

    Rename webapp/ project folder to app/

---
+++ app/src/data/navigation.js
@@ -0,0 +1,8 @@
+
+export const Navigation = [
+    { name: "Home", path: "/", isEnabled: true},
+    { name: "About", path: "/about", isEnabled: true},
+    { name: "Work", path: "/work", isEnabled: true},
+    { name: "Projects", path: "/projects", isEnabled: true},
+    { name: "Blog", path: "/blog", isEnabled: true},
+]
